de3b6ae180255e8217964dda7a512a062c09dc6d
chengfei.feng <chengfei.feng@datayes.com>
2023-03-30 18:25:43 +0800

    eval unary expression and block statement

--- src/main/java/interpreter/eval/Evaluator.java
+++ src/main/java/interpreter/eval/Evaluator.java
@@ -1,10 +1,7 @@
 package interpreter.eval;
 
 import interpreter.ast.*;
-import interpreter.object.BooleanObject;
-import interpreter.object.IntegerObject;
-import interpreter.object.NullObject;
-import interpreter.object.ValueObject;
+import interpreter.object.*;
 import lombok.Data;
 
 /**
@@ -23,9 +20,85 @@ public class Evaluator {
             return new IntegerObject(((IntegerLiteralNode) node).getValue());
         } else if (nodeClass.equals(BooleanLiteralNode.class)) {
             return BooleanObject.getBooleanObject(((BooleanLiteralNode) node).getValue());
+        } else if (nodeClass.equals(UnaryExpressionNode.class)) {
+            return evalUnaryExpression((UnaryExpressionNode) node);
+        } else if (nodeClass.equals(BinaryExpressionNode.class)) {
+            return evalBinaryExpression((BinaryExpressionNode) node);
+        } else if (nodeClass.equals(IfExpressionNode.class)) {
+            return evalIfExpression((IfExpressionNode) node);
+        } else if (nodeClass.equals(BlockStatement.class)) {
+            return evalBlockStatement((BlockStatement) node);
+        } else if (nodeClass.equals(LetStatementNode.class)) {
+            return evalLetStatement((LetStatementNode) node);
+        } else if (nodeClass.equals(FunctionLiteralNode.class)) {
+            return evalFunctionLiteral((FunctionLiteralNode) node);
+        } else if (nodeClass.equals(CallExpressionNode.class)) {
+            return evalCallExpression((CallExpressionNode) node);
+        } else if (nodeClass.equals(ReturnStatementNode.class)) {
+            return evalReturnStatement((ReturnStatementNode) node);
         }
 
         return NullObject.getNullObject();
+    }
+
+    private ValueObject evalFunctionLiteral(FunctionLiteralNode node) {
+        return null;
+    }
+
+    private ValueObject evalCallExpression(CallExpressionNode node) {
+        return null;
+    }
+
+    private ValueObject evalReturnStatement(ReturnStatementNode node) {
+        return null;
+    }
+
+    private ValueObject evalLetStatement(LetStatementNode node) {
+        return null;
+    }
+
+    private ValueObject evalBlockStatement(BlockStatement node) {
+        ValueObject res = null;
+        for (StatementNode statement :
+                node.getStatements()) {
+            res = eval(statement);
+        }
+
+        return res;
+    }
+
+    private ValueObject evalIfExpression(IfExpressionNode node) {
+        return null;
+    }
+
+    private ValueObject evalBinaryExpression(BinaryExpressionNode node) {
+        return null;
+    }
+
+    private ValueObject evalUnaryExpression(UnaryExpressionNode node) {
+        ValueObject res = eval(node.getRight());
+        switch (res.type()) {
+            case INTEGER_OBJ:
+                IntegerObject intValue = (IntegerObject) res;
+                switch (node.getOperator()) {
+                    case "-":
+                        return new IntegerObject(-intValue.getValue());
+                    case "!":
+                        return BooleanObject.getBooleanObject(intValue.getValue() == 0);
+                    default:
+                        return NullObject.getNullObject();
+                }
+            case BOOLEAN_OBJ:
+                BooleanObject boolObj = (BooleanObject) res;
+                switch (node.getOperator()) {
+                    case "!":
+                        return BooleanObject.getBooleanObject(!boolObj.getValue());
+                    default:
+                        return NullObject.getNullObject();
+                }
+            default:
+                return NullObject.getNullObject();
+        }
     }
 
     private ValueObject evalProgram(ProgramNode node) {
